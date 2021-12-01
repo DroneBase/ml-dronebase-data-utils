ad910e84bf76712c7b717f46236b89148f4f7a6e
conorw8 <conorw8@gmail.com>
2021-12-01 15:22:21 -0600

    Basic s3 utils added, README edited

---
+++ dronebase_ml_utils/s3.py
@@ -0,0 +1,73 @@
+import json
+import os
+import boto3
+
+
+def is_json(myjson):
+    """Checks if the string is a json file
+    Args:
+        myjson (str): Filename or path to potential json file.
+    Returns:
+        bool: Whether myjson was a json file.
+    """
+    try:
+        json.loads(myjson)
+    except ValueError:
+        return False
+
+    return True
+
+
+def upload_dir(local_directory, bucket_name, prefix):
+    """Upload data from a local directory to an S3 bucket
+    Args:
+        local_directory (str): Local directory to upload from.
+        bucket_name (str): S3 bucket name.
+        prefix ([type]): Relative path from bucket to save data.
+    """
+    client = boto3.client('s3')
+
+    # enumerate local files recursively
+    for root, dirs, files in os.walk(local_directory):
+        for filename in files:
+            # construct the full local path
+            local_path = os.path.join(root, filename)
+
+            # construct the full Dropbox path
+            relative_path = os.path.relpath(local_path, local_directory)
+            s3_path = os.path.join(prefix, relative_path)
+
+            try:
+                client.head_object(Bucket=bucket_name, Key=s3_path)
+            except ValueError:
+                client.upload_file(local_path, bucket_name, s3_path)
+
+
+def download_s3_file(bucket_name, prefix, local_path):
+    """Download file from S3 bucket to local directory.
+    Args:
+        bucket_name (str): S3 bucket name.
+        prefix ([type]): Relative path from bucket to requested file.
+        local_path ([type]): Local directory to store file.
+    """
+    s3 = boto3.client('s3')
+    s3.download_file(bucket_name, prefix, local_path)
+
+
+def download_s3_folder(bucket_name, prefix, local_dir=None):
+    """Download the contents of a folder directory
+    Args:
+        bucket_name (str): S3 bucket name.
+        prefix (str): Relative path from bucket to requested files.
+        local_dir (str, optional): Local directory to store files in.
+    """
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    for obj in bucket.objects.filter(Prefix=prefix):
+        target = obj.key if local_dir is None \
+            else os.path.join(local_dir, os.path.relpath(obj.key, prefix))
+        if not os.path.exists(os.path.dirname(target)):
+            os.makedirs(os.path.dirname(target))
+        if obj.key[-1] == '/':
+            continue
+        bucket.download_file(obj.key, target)
