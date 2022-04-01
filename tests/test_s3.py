f3ceb52cccf2c12590aa491990dd2c24a327b74f
Conor Wallace <conorwallace@Conors-MacBook-Air.local>
2022-03-31 21:56:54 -0500

    fixed s3 import in tests

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,6 +1,7 @@
 def test_imports():
-    from ml_dronebase_data_utils.s3 import download_s3_file  # noqa: F401
-    from ml_dronebase_data_utils.s3 import download_s3_folder  # noqa: F401
+    from ml_dronebase_data_utils.s3 import download_dir  # noqa: F401
+    from ml_dronebase_data_utils.s3 import download_file  # noqa: F401
     from ml_dronebase_data_utils.s3 import is_json  # noqa: F401
-    from ml_dronebase_data_utils.s3 import sync_s3_folder  # noqa: F401
+    from ml_dronebase_data_utils.s3 import sync_dir  # noqa: F401
     from ml_dronebase_data_utils.s3 import upload_dir  # noqa: F401
+    from ml_dronebase_data_utils.s3 import upload_file  # noqa: F401
