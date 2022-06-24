c1feb687a13d03349c014c85613e8749c88bfabb
Sourav Agrawal <agrawalsourav98@gmail.com>
2022-06-24 10:32:04 +0530

    Added docstring and typedef

--- ml_dronebase_data_utils/convert_geojson.py
+++ ml_dronebase_data_utils/convert_geojson.py
@@ -6,13 +6,26 @@ import rasterio
 from geopandas import GeoDataFrame
 from rasterio.io import DatasetReader
 from tqdm import tqdm
+from typing import Optional, List, Dict
 
 from .box_utils import vertices_to_boxes, vertices_to_rotated_boxes
 from .pascal_voc import PascalVOCWriter
 from .s3 import upload_file
 
 
-def geo_to_voc(ortho_path: str, geo_path: str, save_path: str, class_attribute: str = None, class_mapping:dict = None,default_class:str = 'panel',skip_classes: list =[], rotated: bool = False):
+def geo_to_voc(ortho_path: str, geo_path: str, save_path: str, class_attribute: Optional[str] = None, class_mapping: Optional[Dict[int,str]] = None,default_class:str = 'panel',skip_classes: List[int] =[], rotated: bool = False):
+    '''
+    Convert data on geojson format to pascal voc data.
+
+    :param ortho_path: Path to the ortho. Can be a local/s3 location
+    :param geo_path: Path to the geojson. Can be a local/s3 location
+    :param save_path: Path where the xml file would be saved. Can be a local/s3 location.
+    :param class_attribute: The geojson attribute to be used as the class, e.g. id represents the defect id for current panel
+    :param class_mapping: The class mapping to use. This would map the value of class_attribute to some class
+    :param default_class: The default class to use. Useful when only a single class is being used. Defaults to panel for backwards compatibility.
+    :param skip_classes: The classes to be skipped while the conversion. This should be values from the class_attribute field in the geojson.
+    :param rotated: Specify if to use rotated bounding boxes, defaults to false.
+    '''
     ortho = rasterio.open(ortho_path)
     gdf = gpd.read_file(geo_path)
 
@@ -39,7 +52,8 @@ def geo_to_voc(ortho_path: str, geo_path: str, save_path: str, class_attribute: 
         if name in skip_classes:
             continue
         if class_mapping is not None:
-            name = class_mapping.get(name,default_class)
+            # If mapping is found, use the default name instead of default.
+            name = class_mapping.get(name,name)
         if rotated:
             xmin, ymin, xmax, ymax, angle = box
             writer.addObject(name, xmin, ymin, xmax, ymax, angle)
@@ -55,7 +69,7 @@ def geo_to_voc(ortho_path: str, geo_path: str, save_path: str, class_attribute: 
         writer.save(save_path)
 
 
-def geo_to_rotated_voc(ortho_path: str, geo_path: str, save_path: str, class_attribute: str = None, class_mapping:dict = None,default_class:str = 'panel',skip_classes: list =[]):
+def geo_to_rotated_voc(ortho_path: str, geo_path: str, save_path: str, class_attribute: Optional[str] = None, class_mapping: Optional[Dict[int,str]] = None,default_class:str = 'panel',skip_classes: List[int] =[]):
     # Migrated rotated logic to geo_to_voc and only keeping it for compatibility
     geo_to_voc(ortho_path,geo_path,save_path,class_attribute,class_mapping,default_class,skip_classes,rotated=True)
 
@@ -66,7 +80,7 @@ def get_pixel_vertices(ortho: DatasetReader, gdf: GeoDataFrame) -> np.ndarray:
     geo_vertices = [p.exterior.coords for p in polys]
 
     pxl_vertices = []
-    for _, geo_v in enumerate(tqdm(geo_vertices, total=len(geo_vertices))):
+    for geo_v in tqdm(geo_vertices, total=len(geo_vertices)):
         pxl_v = [ortho.index(c[0], c[1]) for c in geo_v]
         pxl_v = np.asarray(pxl_v)
         # swap axes (y, x) -> (x, y)
