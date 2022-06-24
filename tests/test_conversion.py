50bb888f1505c4dc55f0870f9bed796fab371b5c
Sourav Agrawal <agrawalsourav98@gmail.com>
2022-06-24 12:17:05 +0530

    Added test

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,6 +1,6 @@
 from ml_dronebase_data_utils.convert_geojson import geo_to_voc
 
-if __name__ == "__main__":
+def test_conversion():
     ortho_path = "s3://ml-solar-ortho-fault-detection/orthos/tiff/PA140004_Thermal.tif"
     geojson_path = "s3://ml-solar-ortho-fault-detection/orthos/geojson/PA140004_Thermal.geojson"
     save_path = "s3://ml-solar-ortho-fault-detection/orthos/annotations/PA140004_Thermal.xml"
