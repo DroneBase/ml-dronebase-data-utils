fixed rotation angle calculation and added more rigorous testing.

ml_dronebase_data_utils/box_utils.py
@@ -7,20 +7,38 @@ from shapely.geometry import Polygon
 
 
 def vertices_to_boxes(vertices: np.ndarray) -> np.ndarray:
+    """Convert vertices to boxes.
+
+    Args:
+        vertices (np.ndarray): A Nx4x2 matrix containing the vertices to be converted to boxes.
+
+    Returns:
+        np.ndarray: A Nx4 matrix containing the converted boxes.
+    """
     boxes = []
     for v in vertices:
-        xmin = min(v[:, 0])
-        ymin = min(v[:, 1])
-        xmax = max(v[:, 0])
-        ymax = max(v[:, 1])
-
-        boxes.append([xmin, ymin, xmax, ymax])
+        polygon = Polygon(v)
+        boxes.append(list(polygon.bounds))
 
     return np.asarray(boxes)
 
 
 def vertices_to_rotated_boxes(vertices: np.ndarray) -> np.ndarray:
     """Convert vertices to rotated boxes.
+
+    VOC defined bounding box with orientation angle:
+        [xmin, ymin, xmax, ymax, angle]
+
+    The box coordinates `[xmin, ...]` describe the box when rotated to an upright orientation which
+    is defined when the larger side of the box (the height vector `h`) is parallel to the positive y axis
+    and the shorter side of the box (the width vector `w`) is parallel to he positive x axis.
+
+    e.g.,
+        tan(h) = 0 degrees
+
+    The angle describes the rotation angle required to rotate the box coordinates from an
+    upright orientation to its true orientation.
+    This angle is defined in the range (-90, 90] degrees.
 
     Args:
         vertices (np.ndarray): A Nx4x2 matrix containing the vertices to be converted to boxes.
@@ -41,32 +59,38 @@ def vertices_to_rotated_boxes(vertices: np.ndarray) -> np.ndarray:
         bottom_right = pxl_points_sorted[2]
         bottom_left = pxl_points_sorted[3]
 
-        logging.debug(f"top left = {top_left}")
-        logging.debug(f"top right = {top_right}")
-        logging.debug(f"bottom right = {bottom_right}")
-        logging.debug(f"bottom left = {bottom_left}")
+        logging.info(f"top left = {top_left}")
+        logging.info(f"top right = {top_right}")
+        logging.info(f"bottom right = {bottom_right}")
+        logging.info(f"bottom left = {bottom_left}")
 
-        vec1_mag = np.linalg.norm(bottom_right - top_right)
-        vec2_mag = np.linalg.norm(bottom_right - bottom_left)
+        vec1_mag = np.linalg.norm(top_left - top_right)
+        vec2_mag = np.linalg.norm(top_left - bottom_left)
 
-        if vec1_mag < vec2_mag:
-            if bottom_right[0] <= top_right[0]:
+        if vec1_mag > vec2_mag:
+            h, w = vec1_mag, vec2_mag
+            if top_left[1] > top_right[1]:
+                x1, y1 = top_left
                 x2, y2 = top_right
-                x1, y1 = bottom_right
-                logging.debug(f"point1 = bottom right: {bottom_right}")
-                logging.debug(f"point2 = top right: {top_right}")
+                logging.info(f"point1 = top left: {top_left}")
+                logging.info(f"point2 = top right: {top_right}")
             else:
-                x2, y2 = bottom_right
                 x1, y1 = top_right
-                logging.debug(f"point1 = top right: {top_right}")
-                logging.debug(f"point2 = bottom right: {bottom_right}")
-            w, h = vec1_mag, vec2_mag
+                x2, y2 = top_left
+                logging.info(f"point1 = top right: {top_right}")
+                logging.info(f"point2 = top left: {top_left}")
         else:
-            x2, y2 = bottom_right
-            x1, y1 = bottom_left
-            logging.debug(f"point1 = bottom left: {bottom_left}")
-            logging.debug(f"point2 = bottom right: {bottom_right}")
-            w, h = vec2_mag, vec1_mag
+            h, w = vec2_mag, vec1_mag
+            if top_left[0] >= bottom_left[0]:
+                x1, y1 = bottom_left
+                x2, y2 = top_left
+                logging.info(f"point1 = bottom left: {bottom_left}")
+                logging.info(f"point2 = top left: {top_left}")
+            else:
+                x1, y1 = top_left
+                x2, y2 = bottom_left
+                logging.info(f"point1 = top left: {top_left}")
+                logging.info(f"point2 = bottom left: {bottom_left}")
 
         xmin = xc - w / 2
         ymin = yc - h / 2
@@ -74,9 +98,9 @@ def vertices_to_rotated_boxes(vertices: np.ndarray) -> np.ndarray:
         ymax = yc + h / 2
 
         angle = calculate_angle(x1, y1, x2, y2)
-        logging.debug(f"After angle = {angle}")
-        logging.debug(f"xc, yc, w, h = [{xc}, {yc}, {w}, {h}]")
-        logging.debug(
+        logging.info(f"After angle = {angle}")
+        logging.info(f"xc, yc, w, h = [{xc}, {yc}, {w}, {h}]")
+        logging.info(
             f"xmin, ymin, xmax, ymax, angle = [{xmin}, {ymin}, {xmax}, {ymax}, {angle}]"
         )
 
@@ -87,7 +111,7 @@ def vertices_to_rotated_boxes(vertices: np.ndarray) -> np.ndarray:
 
 def calculate_angle(x1: float, y1: float, x2: float, y2: float) -> float:
     """Compute the orientation angle of a rotated rectangle. `angle` is the
-    angle required to rotate the box from a straight position to its original rotated position.
+    angle required to rotate the box from an upright orientation to its true orientation.
 
     Adapted from detectron2.structures.rotated_boxes
     angle in range: (-90, 90] degrees
@@ -108,9 +132,9 @@ def calculate_angle(x1: float, y1: float, x2: float, y2: float) -> float:
     Returns:
         float: orientation angle
     """
-    angle = np.degrees(np.arctan2(y2 - y1, x2 - x1))
-    logging.debug(f"Before angle = {angle}")
-    if (-90 <= angle) and (angle <= 90):
+    angle = np.degrees(np.arctan2(x1 - x2, y1 - y2))
+    logging.info(f"Before angle = {angle}")
+    if (-90 < angle) and (angle <= 90):
         return angle
     elif angle > 90:
         return angle % 90
@@ -123,13 +147,6 @@ def boxes_to_vertices(boxes: Union[np.ndarray, List[List[float]]]) -> np.ndarray
 
     if not isinstance(boxes, np.ndarray):
         boxes = np.asarray(boxes)
-
-    # Display in largest to smallest order to reduce occlusion.
-    areas = boxes[:, 2] * boxes[:, 3]
-
-    sorted_idxs = np.argsort(-areas).tolist()
-    # Re-order overlapped instances in descending order.
-    boxes = boxes[sorted_idxs]
 
     vertices = []
     for i in range(num_instances):
@@ -193,8 +210,8 @@ def extract_rotated_vertices(
     for k in range(4):
         x_delta = deltas[k][0]
         y_delta = deltas[k][1]
-        x = x_delta * c - y_delta * s + xc
-        y = x_delta * s + y_delta * c + yc
+        x = y_delta * s + x_delta * c + xc
+        y = y_delta * c - x_delta * s + yc
         vertex = (x, y)
         vertices.append(vertex)
     return vertices
@@ -205,12 +222,12 @@ def sort_points(points: np.ndarray) -> np.ndarray:
     coordinates.
 
     left points:
-        top left: left point with max y coordinate
-        bottom left: left point with min y coordinate
+        top left: left point with min y coordinate
+        bottom left: left point with max y coordinate
 
     right points:
-        top right: right point with max y coordinate
-        bottom right: right point with min y coordinate
+        top right: right point with min y coordinate
+        bottom right: right point with max y coordinate
 
     Args:
         points (np.ndarray): A 4x2 matrix of arbitrarily ordered box coordinates.
@@ -222,16 +239,36 @@ def sort_points(points: np.ndarray) -> np.ndarray:
     points_x_sorted = points[np.argsort(points[:, 0])]
 
     if points_x_sorted[1, 0] == points_x_sorted[2, 0]:
-        center_points = points_x_sorted[1:3]
-        points_x_sorted[1] = center_points[np.argmin(center_points[:, 1])]
-        points_x_sorted[2] = center_points[np.argmax(center_points[:, 1])]
+        center_points = points_x_sorted[1:3].copy()
+        logging.info(f"center points before = {center_points}")
+        logging.info(f"center argmax index = {np.argmax(center_points[:, 1])}")
+        logging.info(f"center argmax = {center_points[np.argmax(center_points[:, 1])]}")
+        logging.info(f"point1 before = {points_x_sorted[1]}")
+        bottom_center = center_points[np.argmax(center_points[:, 1])]
+        logging.info(f"point1 after = {points_x_sorted[1]}")
+        logging.info(f"center points after = {center_points}")
+        logging.info(f"center argmin index = {np.argmin(center_points[:, 1])}")
+        logging.info(f"center argmin = {center_points[np.argmin(center_points[:, 1])]}")
+        logging.info(f"point2 before = {points_x_sorted[2]}")
+        top_center = center_points[np.argmin(center_points[:, 1])]
+        logging.info(f"point2 after = {points_x_sorted[2]}")
+        points_x_sorted[1] = bottom_center
+        points_x_sorted[2] = top_center
+    logging.info(f"points x sorted = {points_x_sorted}")
     left_points = points_x_sorted[:2]
     right_points = points_x_sorted[2:]
+    logging.info(f"left points = {left_points}")
+    logging.info(f"right points = {right_points}")
 
-    bottom_left = left_points[np.argmin(left_points[:, 1])]
-    top_left = left_points[np.argmax(left_points[:, 1])]
+    logging.info(f"left argmin = {np.argmin(left_points[:, 1])}")
+    logging.info(f"left argmax = {np.argmax(left_points[:, 1])}")
+    logging.info(f"right argmin = {np.argmin(right_points[:, 1])}")
+    logging.info(f"right argmax = {np.argmax(right_points[:, 1])}")
 
-    bottom_right = right_points[np.argmin(right_points[:, 1])]
-    top_right = right_points[np.argmax(right_points[:, 1])]
+    bottom_left = left_points[np.argmax(left_points[:, 1])]
+    top_left = left_points[np.argmin(left_points[:, 1])]
+
+    bottom_right = right_points[np.argmax(right_points[:, 1])]
+    top_right = right_points[np.argmin(right_points[:, 1])]
 
     return np.stack((top_left, top_right, bottom_right, bottom_left))

ml_dronebase_data_utils/convert_geojson.py
@@ -13,6 +13,17 @@ from .s3 import upload_file
 
 
 def geo_to_voc(ortho_path: str, geo_path: str, save_path: str):
+    """Convert a geojson file containing a set of vertices in geographical coordinates
+    to an xml file containing VOC style bounding boxes in image coordinates.
+
+    Args:
+        ortho_path (str): The path/url to the orthomosaic used to map geographic coordinates
+            to image coordinates. It must have a valid coordinate reference system (CRS) to
+            support coordinate mapping.
+        geo_path (str): The path/url to the geojson file to be converted to. The `geometry` field
+            in the GeoDataFrame is assumed to be Multipolygons.
+        save_path (str): The path/url to save the converted xml file.
+    """
     ortho = rasterio.open(ortho_path)
     gdf = gpd.read_file(geo_path)
 
@@ -34,6 +45,17 @@ def geo_to_voc(ortho_path: str, geo_path: str, save_path: str):
 
 
 def geo_to_rotated_voc(ortho_path: str, geo_path: str, save_path: str):
+    """Convert a geojson file containing a set of vertices in geographical coordinates
+    to an xml file containing oriented VOC style bounding boxes in image coordinates.
+
+    Args:
+        ortho_path (str): The path/url to the orthomosaic used to map geographic coordinates
+            to image coordinates. It must have a valid coordinate reference system (CRS) to
+            support coordinate mapping.
+        geo_path (str): The path/url to the geojson file to be converted to. The `geometry` field
+            in the GeoDataFrame is assumed to contain Multipolygons.
+        save_path (str): The path/url to save the converted xml file.
+    """
     ortho = rasterio.open(ortho_path)
     gdf = gpd.read_file(geo_path)
 
@@ -55,16 +77,36 @@ def geo_to_rotated_voc(ortho_path: str, geo_path: str, save_path: str):
 
 
 def get_pixel_vertices(ortho: DatasetReader, gdf: GeoDataFrame) -> np.ndarray:
+    """Convert the set of geographical vertices to image vertices.
+
+    Args:
+        ortho (DatasetReader): The orthomosaic file used to index geographical coordinates
+            to image coordinates.
+        gdf (GeoDataFrame): The dataframe containing the set of geographical vertices.
+            The `geometry` field is assumed to contain Multipolygons.
+
+    Returns:
+        np.ndarray: A matrix of vertices in image coordinates with shape Nx4x2.
+    """
     gdf = gdf.to_crs(ortho.crs)
     polys = gdf.geometry.explode().tolist()
     geo_vertices = [p.exterior.coords for p in polys]
 
+    import logging
+
     pxl_vertices = []
-    for i, geo_v in enumerate(tqdm(geo_vertices, total=len(geo_vertices))):
+    for geo_v in tqdm(
+        geo_vertices, total=len(geo_vertices), desc="Extracting Pixel Vertices"
+    ):
+        geo_v_ = [[x, y] for x, y in geo_v]
+        geo_v_ = np.asarray(geo_v_)
+        geo_v_[:, [1, 0]] = geo_v_[:, [0, 1]]
+        logging.info(f"get_pixel_vertices:: geo vertices = {geo_v_}")
         pxl_v = [ortho.index(x, y) for x, y in geo_v]
         pxl_v = np.asarray(pxl_v)
         # swap axes (y, x) -> (x, y)
         pxl_v[:, [1, 0]] = pxl_v[:, [0, 1]]
+        logging.info(f"get_pixel_vertices:: pixel vertices = {pxl_v}")
         pxl_vertices.append(pxl_v[:4])
 
     return np.asarray(pxl_vertices)