Fixed rotation angle issue. Added more detailed documentation to relevant functions.

ml_dronebase_data_utils/box_utils.py
@@ -1,6 +1,6 @@
 import logging
 import math
-from typing import List, Union
+from typing import List, Tuple, Union
 
 import numpy as np
 from shapely.geometry import Polygon
@@ -54,53 +54,20 @@ def vertices_to_rotated_boxes(vertices: np.ndarray) -> np.ndarray:
 
         pxl_points_sorted = sort_points(v)
 
-        top_left = pxl_points_sorted[0]
-        top_right = pxl_points_sorted[1]
-        bottom_right = pxl_points_sorted[2]
-        bottom_left = pxl_points_sorted[3]
+        tl = pxl_points_sorted[0]
+        tr = pxl_points_sorted[1]
+        bl = pxl_points_sorted[3]
 
-        logging.info(f"top left = {top_left}")
-        logging.info(f"top right = {top_right}")
-        logging.info(f"bottom right = {bottom_right}")
-        logging.info(f"bottom left = {bottom_left}")
-
-        vec1_mag = np.linalg.norm(top_left - top_right)
-        vec2_mag = np.linalg.norm(top_left - bottom_left)
-
-        if vec1_mag > vec2_mag:
-            h, w = vec1_mag, vec2_mag
-            if top_left[1] > top_right[1]:
-                x1, y1 = top_left
-                x2, y2 = top_right
-                logging.info(f"point1 = top left: {top_left}")
-                logging.info(f"point2 = top right: {top_right}")
-            else:
-                x1, y1 = top_right
-                x2, y2 = top_left
-                logging.info(f"point1 = top right: {top_right}")
-                logging.info(f"point2 = top left: {top_left}")
-        else:
-            h, w = vec2_mag, vec1_mag
-            if top_left[0] >= bottom_left[0]:
-                x1, y1 = bottom_left
-                x2, y2 = top_left
-                logging.info(f"point1 = bottom left: {bottom_left}")
-                logging.info(f"point2 = top left: {top_left}")
-            else:
-                x1, y1 = top_left
-                x2, y2 = bottom_left
-                logging.info(f"point1 = top left: {top_left}")
-                logging.info(f"point2 = bottom left: {bottom_left}")
+        angle, h, w = rotated_box_dims(tl, tr, bl)
 
         xmin = xc - w / 2
         ymin = yc - h / 2
         xmax = xc + w / 2
         ymax = yc + h / 2
 
-        angle = calculate_angle(x1, y1, x2, y2)
-        logging.info(f"After angle = {angle}")
-        logging.info(f"xc, yc, w, h = [{xc}, {yc}, {w}, {h}]")
-        logging.info(
+        logging.debug(f"After angle = {angle}")
+        logging.debug(f"xc, yc, w, h = [{xc}, {yc}, {w}, {h}]")
+        logging.debug(
             f"xmin, ymin, xmax, ymax, angle = [{xmin}, {ymin}, {xmax}, {ymax}, {angle}]"
         )
 
@@ -109,7 +76,84 @@ def vertices_to_rotated_boxes(vertices: np.ndarray) -> np.ndarray:
     return np.asarray(boxes)
 
 
-def calculate_angle(x1: float, y1: float, x2: float, y2: float) -> float:
+def rotated_box_dims(tl: np.ndarray, tr: np.ndarray, bl: np.ndarray) -> Tuple[float]:
+    """Compute the angle, height, and width dimensions of a rotated box given the
+    top left, top right, and bottom left vertices. The angle is the angle between
+    the height vector and the top-down y axis in image space. The height vector is
+    obtained from one of the following conditions:
+
+    If the magnitude of the vector between the top left and top right vertices is larger
+    than the magnitude of the vector between the top left and bottom left vertices,
+
+        e.g., `|| tl - tr || > || tl - bl ||`,
+        h = || tl - tr ||
+
+        1. If the top left coordinate is parallel to or higher than the top right coordinate,
+
+            origin point = top right
+            reference point = top left
+
+            e.g.:
+
+                        tl o
+                           ^
+                            \
+            tl o<--o tr,     \
+                              o tr
+        2. If the top left coordinate is lower than the top right coordinate,
+
+            origin point = top left
+            reference point = top right
+
+            e.g.:
+
+                  o tr
+                  ^
+                 /
+                /
+            tl o
+
+    Otherwise, the height vector is between the top left and bottom left vertices,
+
+    3. In this case, the height vector is always oriented from the bottom left to top left vertices,
+
+    e.g.:
+
+    tl o               o tl
+       ^               ^
+        \             /                                                                 # noqa: W605
+         \     ,     /                                                                  # noqa: W605
+          o bl   bl o
+
+    Args:
+        tl (np.ndarray): _description_
+        tr (np.ndarray): _description_
+        bl (np.ndarray): _description_
+
+    Returns:
+        Tuple[float]: _description_
+    """
+    v1 = np.linalg.norm(tl - tr)
+    v2 = np.linalg.norm(tl - bl)
+
+    if (v1 > v2) and (tl[1] <= tr[1]):
+        logging.debug(f"point1 = top right: {tr}")
+        logging.debug(f"point2 = top left: {tl}")
+        angle = calculate_angle(tr, tl)
+        return angle, v1, v2
+    elif (v1 > v2) and (tl[1] > tr[1]):
+        logging.debug(f"point1 = top left: {tl}")
+        logging.debug(f"point2 = top right: {tr}")
+        angle = calculate_angle(tl, tr)
+        return angle, v1, v2
+    else:
+        logging.debug(f"point1 = bottom left: {bl}")
+        logging.debug(f"point2 = top left: {tl}")
+        angle = calculate_angle(bl, tl)
+        return angle, v2, v1
+
+
+def calculate_angle(point1: np.ndarray, point2: np.ndarray) -> float:
     """Compute the orientation angle of a rotated rectangle. `angle` is the
     angle required to rotate the box from an upright orientation to its true orientation.
 
@@ -124,16 +168,16 @@ def calculate_angle(x1: float, y1: float, x2: float, y2: float) -> float:
         box_rotated is obtained by rotating box w.r.t its center by :math:`-|angle| % 90` degrees CCW.
 
     Args:
-        x1 (float): x coordinate of rotation point
-        y1 (float): y coordinate of rotation point
-        x2 (float): x coordinate of reference point
-        y2 (float): y coordinate of reference point
+        point1 (np.ndarray): x, y coordinates of origin rotation point
+        point2 (np.ndarray): x, y coordinates of reference rotation point
 
     Returns:
         float: orientation angle
     """
+    x1, y1 = point1
+    x2, y2 = point2
     angle = np.degrees(np.arctan2(x1 - x2, y1 - y2))
-    logging.info(f"Before angle = {angle}")
+    logging.debug(f"Before angle = {angle}")
     if (-90 < angle) and (angle <= 90):
         return angle
     elif angle > 90:
@@ -240,35 +284,23 @@ def sort_points(points: np.ndarray) -> np.ndarray:
 
     if points_x_sorted[1, 0] == points_x_sorted[2, 0]:
         center_points = points_x_sorted[1:3].copy()
-        logging.info(f"center points before = {center_points}")
-        logging.info(f"center argmax index = {np.argmax(center_points[:, 1])}")
-        logging.info(f"center argmax = {center_points[np.argmax(center_points[:, 1])]}")
-        logging.info(f"point1 before = {points_x_sorted[1]}")
         bottom_center = center_points[np.argmax(center_points[:, 1])]
-        logging.info(f"point1 after = {points_x_sorted[1]}")
-        logging.info(f"center points after = {center_points}")
-        logging.info(f"center argmin index = {np.argmin(center_points[:, 1])}")
-        logging.info(f"center argmin = {center_points[np.argmin(center_points[:, 1])]}")
-        logging.info(f"point2 before = {points_x_sorted[2]}")
         top_center = center_points[np.argmin(center_points[:, 1])]
-        logging.info(f"point2 after = {points_x_sorted[2]}")
         points_x_sorted[1] = bottom_center
         points_x_sorted[2] = top_center
-    logging.info(f"points x sorted = {points_x_sorted}")
+
     left_points = points_x_sorted[:2]
     right_points = points_x_sorted[2:]
-    logging.info(f"left points = {left_points}")
-    logging.info(f"right points = {right_points}")
-
-    logging.info(f"left argmin = {np.argmin(left_points[:, 1])}")
-    logging.info(f"left argmax = {np.argmax(left_points[:, 1])}")
-    logging.info(f"right argmin = {np.argmin(right_points[:, 1])}")
-    logging.info(f"right argmax = {np.argmax(right_points[:, 1])}")
 
     bottom_left = left_points[np.argmax(left_points[:, 1])]
     top_left = left_points[np.argmin(left_points[:, 1])]
 
     bottom_right = right_points[np.argmax(right_points[:, 1])]
     top_right = right_points[np.argmin(right_points[:, 1])]
+
+    logging.debug(f"top left = {top_left}")
+    logging.debug(f"top right = {top_right}")
+    logging.debug(f"bottom right = {bottom_right}")
+    logging.debug(f"bottom left = {bottom_left}")
 
     return np.stack((top_left, top_right, bottom_right, bottom_left))